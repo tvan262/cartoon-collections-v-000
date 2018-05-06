5e3f2e0be821ea07c5c1b6808ca5450de6956db1
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 04:15:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,8 @@ end
 
 def summon_captain_planet(planeteer_calls)             # code an argument here
   # Your code here
+  
+  
 end
 
 def long_planeteer_calls(array)           # code an argument here
