2dbd416b59e0c2747f6aa56db2b0f8382ad64905
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 04:17:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,12 @@ def summon_captain_planet(planeteer_calls)             # code an argument here
   end
 end
 
-def long_planeteer_calls(array)           # code an argument here
+def long_planeteer_calls(call)           # code an argument here
   # Your code here
+  
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese(cheese_types)         # code an argument here
