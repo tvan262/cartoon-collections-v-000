8adb23cfb14688a3f6934d3dbee8e39f92ff62ed
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 04:15:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(dwarves)       # code an argument here
   # Your code here
-  def roll_call_dwarves(dwarves)
+
   dwarves.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
