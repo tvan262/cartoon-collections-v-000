b16ae4cc74fd29767cc3f8f37de0ec1e9c490c09
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 04:10:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,15 +7,15 @@ def roll_call_dwarves(array)# code an argument here
  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)             # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)           # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)         # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
