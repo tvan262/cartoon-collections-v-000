c56db02b0d08881eac9b7d32f04c60aaa21b93d4
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 04:03:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 0
+  array.each do |array|
+    puts "#{name}"
+    i = i + 1
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +18,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+end 
